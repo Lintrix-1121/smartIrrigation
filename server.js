32236f0a4746fb40d53efdf7333ec3d65eb1ffd8
lintrix <lintrix@Lintrix.lintrix.com>
2026-04-08 16:09:17 +0300

    .env update for secretKey

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
