2a8f7b67000fe00a3fec63e7fb4c61c0e84ad2d5
lintrix <lintrix@Lintrix.lintrix.com>
2026-07-17 12:22:57 +0300

    method route alignment

--- routes/irrigationMethods.route.js
+++ routes/irrigationMethods.route.js
@@ -5,7 +5,7 @@ module.exports = app => {
     var router = require("express").Router();
 
     router.post('/method', methods_controller.MethodData);
-    router.get('/methods', methods_controller.GetData);
+    router.get('/mtds', methods_controller.GetData);
 
     app.use('/api', router);
 }
